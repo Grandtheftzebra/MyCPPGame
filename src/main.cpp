39a1de47f8494e78fbbb531906a782e714962481
marcr <marc.rydzy@gmail.com>
2026-03-18 00:48:01 +0100

    added gitignore and first commit

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::cout << "Hello CMake World" << std::endl;
+
+    if (!PRODUCTION_BUILD)
+    {
+        std::cout << "This is a debug build" << std::endl;
+    }
+
+    return 0;
+}
