dea3b29e0b3deab31ff9fd99f84c3229c2964b0e
marcr <marc.rydzy@gmail.com>
2026-03-18 20:05:22 +0100

    added raylib and toyed a little with it

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,26 @@
 ﻿#include <iostream>
+#include <raylib.h>
 
 int main()
 {
-    std::cout << "Hello CMake World" << std::endl;
 
-    if (!PRODUCTION_BUILD)
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+    InitWindow(800, 450, "My Game");
+
+    while (!WindowShouldClose())
     {
-        std::cout << "This is a debug build" << std::endl;
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+
+        DrawText("My first text", 400, 0, 20, GREEN);
+
+        //DrawRectangle(100, 100, 200, 200, {50,200,200, 255});
+        DrawRectangle(100, 100, 200, 200, {200,0,200, 100});
+
+        EndDrawing();
     }
+
+    CloseWindow();
 
     return 0;
 }
