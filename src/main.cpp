83361a4eb32e1a7d52d96685f8b9f3b3ab2dc2ab
marcr <marc.rydzy@gmail.com>
2026-03-20 00:15:48 +0100

    added rlImgui and updated CMakeLists.txt

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,40 @@
 ﻿#include <iostream>
 #include <raylib.h>
 
+#include <imgui.h>
+#include <rlImGui.h>
+
 int main()
 {
 
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
     InitWindow(800, 450, "My Game");
 
+    rlImGuiSetup(true);
+
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(RAYWHITE);
+        rlImGuiBegin();
 
         DrawText("My first text", 400, 0, 20, GREEN);
 
-        //DrawRectangle(100, 100, 200, 200, {50,200,200, 255});
+        DrawRectangle(100, 100, 200, 200, {50,200,200, 255});
         DrawRectangle(100, 100, 200, 200, {200,0,200, 100});
 
+        ImGui::Begin("My first window");
+
+        ImGui::Text("Hello World");
+        ImGui::Button("Click me");
+
+        ImGui::End();
+
+        rlImGuiEnd();
         EndDrawing();
     }
 
+    rlImGuiShutdown();
     CloseWindow();
 
     return 0;
